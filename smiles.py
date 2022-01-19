dfee26b45bddeab60c3aec8ac898a98685f9e57c
root <root@Anime-Machine.localdomain>
2022-01-19 10:45:47 -0700

    updated sidebar functionality

--- smiles.py
+++ smiles.py
@@ -6,7 +6,6 @@ from rdkit import Chem
 from rdkit.Chem import Draw
 from rdkit.Chem import AllChem
 import hydralit_components as hc
-import hydralit as hy
 
 # @st.cache(allow_output_mutation=True)
 # def smile_models():
@@ -16,9 +15,7 @@ import hydralit as hy
 # def smile_strings():
 #     return []
 
-app = hy.HydraApp(title='Simple Multi-Page App')
-
-# st.set_page_config(layout='wide',initial_sidebar_state='collapsed',)
+st.set_page_config(layout='wide',initial_sidebar_state='collapsed',)
 
 menu_data = [
     {'icon': "far fa-copy", 'label':"Left End"},
@@ -37,85 +34,84 @@ menu_id = hc.nav_bar(
     sticky_mode='pinned', #jumpy or not-jumpy, but sticky or pinned
 )
 
-@app.addapp()
-def my_home():
+st.title('SMILES  + RDKit + Py3DMOL :smiley:')
 
-    st.title('SMILES  + RDKit + Py3DMOL :smiley:')
+ss = SessionState.get(smile_models={}, smile_strings=[])
 
-    ss = SessionState.get(smile_models={}, smile_strings=[])
+def get_pymol_style(style):
+    return style
 
-    def get_pymol_style(style):
-        return style
+pymol_style = st.selectbox('Select 3D Style', ('stick','line','sphere'))
 
-    pymol_style = st.selectbox('Select 3D Style', ('stick','line','sphere'))
+def create_models(smi, style=get_pymol_style(pymol_style)):
+    mol = Chem.MolFromSmiles(smi)
+    mol = Chem.AddHs(mol)
+    AllChem.EmbedMolecule(mol)
+    AllChem.MMFFOptimizeMolecule(mol, maxIters=200)
+    mblock = Chem.MolToMolBlock(mol)
+    view = py3Dmol.view(width=450, height=450)
+    view.addModel(mblock, 'mol')
+    view.setStyle({style:{}})
+    view.zoomTo()
+    view.show()
+    view.render()
+    t = view.js()
+    f = open('viz.html', 'w')
+    f.write(t.startjs)
+    f.write(t.endjs)
+    f.close()
+    
+def add_smiles(input_smiles):
+    """Take the smile the user input and add it to the list of smile strings
+    along with their 2d and 3d structure.
 
-    def create_models(smi, style=get_pymol_style(pymol_style)):
-        mol = Chem.MolFromSmiles(smi)
-        mol = Chem.AddHs(mol)
-        AllChem.EmbedMolecule(mol)
-        AllChem.MMFFOptimizeMolecule(mol, maxIters=200)
-        mblock = Chem.MolToMolBlock(mol)
-        view = py3Dmol.view(width=450, height=450)
-        view.addModel(mblock, 'mol')
-        view.setStyle({style:{}})
-        view.zoomTo()
-        view.show()
-        view.render()
-        t = view.js()
-        f = open('viz.html', 'w')
-        f.write(t.startjs)
-        f.write(t.endjs)
-        f.close()
-        
-    def add_smiles(input_smiles):
-        """Take the smile the user input and add it to the list of smile strings
-        along with their 2d and 3d structure.
+    Args:
+        input_smiles (str): The string input of a smile provided by the user
+    """
+    m = Chem.MolFromSmiles(input_smiles)
+    create_models(input_smiles)
+    HtmlFile = open("viz.html", 'r', encoding='utf-8')
+    source_code = HtmlFile.read()
+    
+    if input_smiles not in ss.smile_strings:
+        ss.smile_strings.append(input_smiles)
+        ss.smile_models[input_smiles] = (m, source_code)
 
-        Args:
-            input_smiles (str): The string input of a smile provided by the user
-        """
-        m = Chem.MolFromSmiles(input_smiles)
-        create_models(input_smiles)
-        HtmlFile = open("viz.html", 'r', encoding='utf-8')
-        source_code = HtmlFile.read()
-        
-        if input_smiles not in ss.smile_strings:
-            ss.smile_strings.append(input_smiles)
-            ss.smile_models[input_smiles] = (m, source_code)
+def display_smiles(first_smile, second_smile):
+    
+    c1,c2=st.columns(2)
+    with c1:
+        Draw.MolToFile(ss.smile_models[first_smile][0], 'mol.png')
+        st.image('mol.png')
+    with c2:
+        components.html(ss.smile_models[first_smile][1],height=450,width=450)
+    
+    # print("first:", first_smile, "\tsecond:", second_smile)
+    
+    if first_smile == second_smile:
+        return
+    
+    c1,c2=st.columns(2)
+    with c1:
+        Draw.MolToFile(ss.smile_models[second_smile][0], 'mol2.png')
+        st.image('mol2.png')
+    with c2:
+        components.html(ss.smile_models[second_smile][1], height=450,width=450)
 
-    def display_smiles(first_smile, second_smile):
-        
-        c1,c2=st.columns(2)
-        with c1:
-            Draw.MolToFile(ss.smile_models[first_smile][0], 'mol.png')
-            st.image('mol.png')
-        with c2:
-            components.html(ss.smile_models[first_smile][1],height=450,width=450)
-            
-        c1,c2=st.columns(2)
-        with c1:
-            Draw.MolToFile(ss.smile_models[second_smile][0], 'mol2.png')
-            st.image('mol2.png')
-        with c2:
-            components.html(ss.smile_models[second_smile][1], height=450,width=450)
+input_smiles=st.text_input('Enter SMILES string\nLeft click, hold, then move mouse to rotate 3D view. \
+                            Right click, hold then move mouse to zoom in/out.',\
+                            'O=C1C2=C(N=CN2C)N(C)C(N1C)=O')
+print(input_smiles) # For debugging
+try:
+    add_smiles(input_smiles)
+except BaseException:
+        st.write('Invalid SMILES, please input a valid SMILES string.')
+    
+################ Sidebar ####################
+second_index=[len(ss.smile_strings)-2 if len(ss.smile_strings) > 1 else 0][0]
+first_smile = st.sidebar.selectbox('Select your first desired SMILE', ss.smile_strings, index=len(ss.smile_strings)-1)
+second_smile = st.sidebar.selectbox('Select your second desired SMILE', ss.smile_strings, index=second_index)
 
-    input_smiles=st.text_input('Enter SMILES string\nLeft click, hold, then move mouse to rotate 3D view. \
-                                Right click, hold then move mouse to zoom in/out.',\
-                                'O=C1C2=C(N=CN2C)N(C)C(N1C)=O')
-    print(input_smiles) # For debugging
-    try:
-        add_smiles(input_smiles)
-    except BaseException:
-            st.write('Invalid SMILES, please input a valid SMILES string.')
-        
-    ################ Sidebar ####################
-    first_smile = st.sidebar.selectbox('Select your first desired SMILE', ss.smile_strings)
-    second_smile = st.sidebar.selectbox('Select your second desired SMILE', ss.smile_strings)
-    if len(ss.smile_strings) >= 2:
-        display_smiles(first_smile, second_smile)
 
-@app.addapp()
-def app2():
- hy.info('Hello from app 2')
- 
- app.run()
+
+display_smiles(first_smile, second_smile)
