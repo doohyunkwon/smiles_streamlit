59c0c07b953d0524995414d35fff95178ff0437a
Kavika <kavikafaleumu18@gmail.com>
2022-01-26 10:56:45 -0700

    reverted back to old working version, style menu works

--- smiles.py
+++ smiles.py
@@ -43,9 +43,13 @@ ss = SessionState.get(smile_models={}, smile_strings=[])
 def get_pymol_style(style):
     return style
 
-pymol_style = st.selectbox('Select 3D Style', ('stick','line','sphere'))
+def generate_HTML_name(style):
+    return "viz_" + style + ".html"
 
-def create_models(smi, style=get_pymol_style(pymol_style)):
+styles = ('stick','line','sphere')
+pymol_style = st.selectbox('Select 3D Style', styles)
+
+def create_models(smi, style):
     mol = Chem.MolFromSmiles(smi)
     mol = Chem.AddHs(mol)
     AllChem.EmbedMolecule(mol)
@@ -58,7 +62,8 @@ def create_models(smi, style=get_pymol_style(pymol_style)):
     view.show()
     view.render()
     t = view.js()
-    f = open('viz.html', 'w')
+    html_name = generate_HTML_name(style)
+    f = open(html_name, 'w')
     f.write(t.startjs)
     f.write(t.endjs)
     f.close()
@@ -70,14 +75,21 @@ def add_smiles(input_smiles):
     Args:
         input_smiles (str): The string input of a smile provided by the user
     """
+    if input_smiles in ss.smile_strings:
+        return
     m = Chem.MolFromSmiles(input_smiles)
-    create_models(input_smiles)
-    HtmlFile = open("viz.html", 'r', encoding='utf-8')
-    source_code = HtmlFile.read()
+    ss.smile_strings.append(input_smiles)
+    ss.smile_models[input_smiles] = (m, {})
     
-    if input_smiles not in ss.smile_strings:
-        ss.smile_strings.append(input_smiles)
-        ss.smile_models[input_smiles] = (m, source_code)
+    for style in styles:
+        create_models(input_smiles, style)
+        html_name = generate_HTML_name(style)
+        HtmlFile = open(html_name, 'r', encoding='utf-8')
+        source_code = HtmlFile.read()
+        ss.smile_models[input_smiles][1][style] = source_code
+
+        
+        
 
 def display_smiles(first_smile, second_smile):
     
@@ -86,7 +98,7 @@ def display_smiles(first_smile, second_smile):
         Draw.MolToFile(ss.smile_models[first_smile][0], 'mol.png')
         st.image('mol.png')
     with c2:
-        components.html(ss.smile_models[first_smile][1],height=450,width=450)
+        components.html(ss.smile_models[first_smile][1][pymol_style],height=450,width=450)
     
     # print("first:", first_smile, "\tsecond:", second_smile)
     
@@ -98,7 +110,7 @@ def display_smiles(first_smile, second_smile):
         Draw.MolToFile(ss.smile_models[second_smile][0], 'mol2.png')
         st.image('mol2.png')
     with c2:
-        components.html(ss.smile_models[second_smile][1], height=450,width=450)
+        components.html(ss.smile_models[second_smile][1][pymol_style], height=450,width=450)
 
 input_smiles=st.text_input('Enter SMILES string\nLeft click, hold, then move mouse to rotate 3D view. \
                             Right click, hold then move mouse to zoom in/out.',\
@@ -113,7 +125,5 @@ except BaseException:
 second_index=[len(ss.smile_strings)-2 if len(ss.smile_strings) > 1 else 0][0]
 first_smile = st.sidebar.selectbox('Select your first desired SMILE', ss.smile_strings, index=len(ss.smile_strings)-1)
 second_smile = st.sidebar.selectbox('Select your second desired SMILE', ss.smile_strings, index=second_index)
-
-
 
 display_smiles(first_smile, second_smile)
